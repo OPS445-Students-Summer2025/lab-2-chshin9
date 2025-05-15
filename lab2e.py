adff6580787096d57ef9f3c878abe385585c6bc5
Charles Shin <cshin9@myseneca.ca>
2025-05-15 09:38:21 -0400

    Finished lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Charles Shin
+#Author ID: 166315226
+#Date Created: 2025/05/8
+
+timer = 10
+
+while timer != 0:
+    print(str(timer))
+    timer = timer - 1
+
+print("blast off!")
