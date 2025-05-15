d70f2514e50934c8a59b7c0b74a68cbf65fbaf0e
Charles Shin <cshin9@myseneca.ca>
2025-05-15 09:38:32 -0400

    Finished lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Charles Shin
+#Author ID: 166315226
+#Date Created: 2025/05/8
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(str(timer))
+    timer = timer - 1
+
+print("blast off!")
